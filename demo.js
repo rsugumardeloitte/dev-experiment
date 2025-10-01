1a020dd51f40ddcbc83fc901a49f28285c615809
rsugumardeloitte <rsugumar@deloitte.ca>
2025-09-30 21:09:03 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-const password = "12345-Secret";
+const AWS_SECRET_ACCESS_KEY = "12345-Secret";
 eval("console.log('Insecure')");
 // Vulnerable version of DOMPurify in use
 const dirty = "<img src=x onerror=alert('XSS')>";
