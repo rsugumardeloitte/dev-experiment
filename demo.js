69b6e4c145e97706f20e09547474482baf0e8735
rsugumardeloitte <rsugumar@deloitte.ca>
2025-10-01 12:57:41 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,6 +4,6 @@ const dirty = "<img src=x onerror=alert('XSS')>";
 document.getElementById("output").innerHTML = DOMPurify.sanitize(dirty);
 
 function showMessage() {
-    alert("Hello from demo.js!");
+    alert("Hello!");
 }
 
