7c814d5cff3a6a5bac997dddbd2e51ff771b6146
rsugumardeloitte <rsugumar@deloitte.ca>
2025-09-30 21:15:31 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
-const AWS_SECRET_ACCESS_KEY = "12345-Secret";
-eval("console.log('Insecure')");
 // Vulnerable version of DOMPurify in use
 const dirty = "<img src=x onerror=alert('XSS')>";
 document.getElementById("output").innerHTML = DOMPurify.sanitize(dirty);
+
+function processData(input) {
+  // This line is intentionally vulnerable to be flagged by Semgrep
+  eval("console.log('" + input + "');");
+}
+
+processData("Hello, World!");
