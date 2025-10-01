62e6fb630acdab85104bd2ecc84fb80d74a39e81
rsugumardeloitte <rsugumar@deloitte.ca>
2025-10-01 12:44:17 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,14 +3,7 @@
 //const dirty = "<img src=x onerror=alert('XSS')>";
 document.getElementById("output").innerHTML = DOMPurify.sanitize(dirty);
 
-// Example: intentionally introduce a JSHint error
-// (like a missing semicolon or undefined variable)
-//hello.setcups.demo = "This will fail"; // <-- JSHint will flag this
-
-// Function to display a message
-function showMessage(message) {
-    document.getElementById('output').textContent = message;
+function showMessage() {
+    alert("Hello from demo.js!");
 }
 
-// Call the function
-showMessage("JavaScript is running!"); 
