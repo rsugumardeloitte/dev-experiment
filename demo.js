3250f4d991387cb55b497dcd6dc7c97ebc228f2f
rsugumardeloitte <rsugumar@deloitte.ca>
2025-10-01 12:08:59 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
 //DOMPurify in use
 
-const dirty = "<img src=x onerror=alert('XSS')>";
+//const dirty = "<img src=x onerror=alert('XSS')>";
 document.getElementById("output").innerHTML = DOMPurify.sanitize(dirty);
